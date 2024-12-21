a331857d9eb80b1c473064fff5b0d8396e3cc607
md-alif-jasim <mdalifjasim@gmail.com>
2024-12-21 18:07:06 -0500

    homepage pic added

---
+++ src/Components/Hero/Hero.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Hero.css';
+
+const Hero = () => {
+  return (
+    <div className="hero">
+      <div className="hero-content">
+        <button className="btn">Get Started</button>
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
