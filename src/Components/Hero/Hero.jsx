94634263e3e223ac5f9110c426461f7fcdfd067c
md-alif-jasim <mdalifjasim@gmail.com>
2024-12-21 18:46:36 -0500

    dark arrow added

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -1,11 +1,17 @@
 import React from 'react';
 import './Hero.css';
+import dark_arrow from "../../assets/dark-arrow.png";
+
 
 const Hero = () => {
   return (
-    <div className="hero">
-      <div className="hero-content">
-        <button className="btn">Get Started</button>
+    <div className="hero container">
+      <div className="hero-text">
+        
+        <h1>We ensure better Education for a better world</h1>
+        <p>Our cutting edge curriculam is designed to empower students with knowledge, skills and experiencesneeded to excel in the dynamic field of education.</p>
+        <button className='btn'> Explore More <img src={dark_arrow} alt="" /></button>
+      
       </div>
     </div>
   );
