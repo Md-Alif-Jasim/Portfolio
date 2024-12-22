25037282e8420946d224affa1c23b8cc8cbd75fc
md-alif-jasim <mdalifjasim@gmail.com>
2024-12-21 21:34:54 -0500

    Pics gets highlighted when gets hovered

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,12 @@ const App = () => {
     <div>
       <Navbar/>
       <Hero />
-      <Programs/>
+      
+      <div className='container'>
+        <Programs/>
+        
+      </div>
+      
     </div>
   )
 }
